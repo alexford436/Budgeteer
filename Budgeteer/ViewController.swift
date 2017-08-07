9567351af1f6d30c6432e044836a072e2282cdab
Alex Ford <aford3532@gmail.com>
2017-08-07 12:20:26 -0400

    Initial Commit

---
+++ Budgeteer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Budgeteer
+//
+//  Created by Alex Ford on 2017-08-07.
+//  Copyright © 2017 Alex Ford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
